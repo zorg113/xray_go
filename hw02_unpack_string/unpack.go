8a3d07f8665f910357140736599f8950d9edf081
dan <dmitryina@gmail.com>
2024-10-14 13:07:41 +0300

    fix: state use enum

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -7,21 +7,32 @@ import (
 	"unicode"
 )
 
+var ErrInvalidString = errors.New("invalid string")
+
+type States int
+
+const (
+	Empty States = iota
+	Symbol
+)
+
 type stateMashine struct {
-	state int             // варианты состояния и переходов 1 и 0
+	state States
 	prev  rune            // запоминаемый символ
 	out   strings.Builder // распакованная сторка
 }
 
 func newSateMashine() *stateMashine {
-	return &stateMashine{}
+	return &stateMashine{state: Empty}
 }
+
+// поверка входного символа на число и распаковка
 
 func (m *stateMashine) checkDigit(s rune) error {
 	n := 0
 	err := error(nil)
 	if unicode.IsDigit(s) {
-		if m.state == 0 {
+		if m.state == Empty {
 			return ErrInvalidString
 		}
 		if n, err = strconv.Atoi(string(s)); err != nil {
@@ -32,34 +43,40 @@ func (m *stateMashine) checkDigit(s rune) error {
 				return err
 			}
 		}
-		m.state = 0
+		m.state = Empty
 	}
 	return nil
 }
 
+// проврка на нечисловой символ и сохранение его для
+// возможной распаковки
+
 func (m *stateMashine) checkSymbol(s rune) error {
 	if !unicode.IsDigit(s) {
-		if m.state == 1 {
+		if m.state == Symbol {
 			if _, err := m.out.WriteRune(m.prev); err != nil {
 				return err
 			}
 		}
 		m.prev = s
-		m.state = 1
+		m.state = Symbol
 	}
 	return nil
 }
 
+// сброс последнего состояния и вывод сохраненного последнего символа
+
 func (m *stateMashine) end() error {
-	if m.state == 1 {
+	if m.state == Symbol {
 		if _, err := m.out.WriteRune(m.prev); err != nil {
 			return err
 		}
+		m.state = Empty
 	}
 	return nil
 }
 
-var ErrInvalidString = errors.New("invalid string")
+// распаковка строк
 
 func Unpack(s string) (string, error) {
 	extract := newSateMashine()
